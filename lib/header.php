41c3a64fd35d6991d090cea54771e7224e81708e
Derick Estephen <derickestephen1999@gmail.com>
2023-09-03 21:20:09 +0500

    Add new CDNs

--- lib/header.php
+++ lib/header.php
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>First Project with GitHub</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    container: {
+                        center: true,
+                        padding: {
+                            DEFAULT: "1rem",
+                            md: "1.5rem",
+                            lg: "2rem",
+                            xl: "2.5rem"
+                        }
+                    },
+                    colors: {
+                        clifford: '#da373d',
+                    }
+                }
+            }
+        }
+    </script>
 </head>
+
 <body>
-    
